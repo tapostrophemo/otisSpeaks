41d4517dcc66d0391fe646b0efbb8b2886054116
Dan Parks <dan.parks@leandog.com>
2014-01-28 17:55:10 -0500

    rudimentary translation to dog-speak

--- app.rb
+++ app.rb
@@ -1,10 +1,27 @@
 require 'sinatra'
 
+module WordUtils
+  DOG_WURDZ = {
+    'a' => 'aaarrr',
+    'e' => 'rrrrreeee',
+    'i' => 'yeeeelp',
+    'o' => 'aarrroooooo',
+    'u' => 'wooof',
+  }
+
+  def to_dogspeak(str)
+    vowels = str.gsub(/[^aeiou ]/, '').split(' ')
+    vowels.map {|v| DOG_WURDZ[v[0]]}.join(' ')
+  end
+end
+
+helpers WordUtils
+
 get '/' do
   erb :main, locals: {text: 'woof! woof!'}
 end
 
 post '/' do
-  sayit = params[:sayit]
+  sayit = to_dogspeak(params[:sayit])
   erb :main, locals: {text: "rooof! #{sayit}"}
 end
